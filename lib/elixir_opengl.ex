2cb248f16fda40ae54757c3c17dfb4d0aa74d184
Ian Harris <ian@fofgof.xyz>
2024-05-07 10:34:31 -0700

    change background color to purple
    elixir

--- lib/elixir_opengl.ex
+++ lib/elixir_opengl.ex
@@ -131,7 +131,7 @@ void main() {
   end
 
   defp draw(%{shader_program: shader_program, vao: vao} = _state) do
-    :gl.clearColor(0.1, 0.2, 0.3, 1.0)
+    :gl.clearColor(0.2, 0.1, 0.3, 1.0)
     :gl.clear(:gl_const.gl_color_buffer_bit)
 
     :gl.useProgram(shader_program)
